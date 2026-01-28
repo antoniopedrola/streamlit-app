108873b86203012486cca575a34a51df34566b9f
Antonio Pedrola <Antonio.Pedrola@HC94M99VHT.irisnation.com>
2026-01-28 10:20:30 +0000

    first app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,12 @@ import streamlit as st
 
 st.title("Persona-Aware Interview Assistant")
 
+
+personas = ["Alex", "Jamie", "Sam"]
+
 persona = st.selectbox("Select persona", personas)
+
+st.write("Selected persona:", persona)
 
 query = st.chat_input("Ask a question")
 
